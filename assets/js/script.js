9e816bbe63fee03cf0886fd607f4f03f5fa9c540
IsaiasQB <isaias.qb@hotmail.com>
2022-03-31 12:32:25 -0400

    modified google API call to obtain books by the title

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,28 @@
-// let input = "harrypotter";
-// fetch(`https://www.googleapis.com/books/v1/volumes/?q=${input}`).then(
-//   (response) => {
-//     response.json().then((data) => {
-//         console.log("gOOGLE DATA");
-//         console.log(data);
-//     });
-//   }
-// );
+let input = "love";
+fetch(`https://www.googleapis.com/books/v1/volumes/?q=${input}`).then(
+  (response) => {
+    response.json().then((data) => {
+        console.log("gOOGLE DATA");
+        console.log(data);
+    });
+  }
+);
+
+fetch("https://www.googleapis.com/books/v1/volumes?q=intitle:life+after").then(
+    (response) => {
+      response.json().then((data) => {
+          console.log("gOOGLE DATA in tittle");
+          console.log(data);
+      });
+    }
+  );
+
 
 let key = "clDs6fwfqKr0Y0PRfKe3UbdrkvPt6wwd";
-fetch("https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key="+key).then(
+
+var bookName = 
+
+fetch("https://api.nytimes.com/svc/books/v3/reviews.json?title=life&api-key="+key).then(
   (response) => {
     response.json().then((data) => {
         console.log("NYT DATA");
