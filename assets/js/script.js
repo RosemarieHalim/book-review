7c16d996de6bac44764fc5f727912259353f4b8d
IsaiasQB <isaias.qb@hotmail.com>
2022-03-31 20:48:04 -0400

    reworking API calls into 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,23 @@
 
 
-// example of an API call from google
+    // example of an API call from google
 fetch("https://www.googleapis.com/books/v1/volumes?q=intitle:philosopher+stone&printType=books&maxResults=40").then(
     (response) => {
       response.json().then((data) => {
-          console.log("gOOGLE DATA in tittle");
+          console.log("GOOGLE DATA seach by title");
           console.log(data);
+          var bookId = data.items[0].volumeInfo.industryIdentifiers[0].identifier
+          console.log(bookId)
+
+            //example of an API call from open library using ISBN:9780756413712 identifier as parameter
+        fetch("https://openlibrary.org/api/books?bibkeys=ISBN:"+bookId+"&jscmd=details&format=json").then(
+            (response) => {
+            response.json().then((data) => {
+                console.log("OPEN LIBRARY DATA");
+                console.log(data);
+            });
+            }
+        );
       });
     }
   );
-
-  
-// example of an API call from open library using ISBN:9780756413712 identifier as parameter
-fetch("https://openlibrary.org/api/books?bibkeys=ISBN:9780756413712&jscmd=details&format=json").then(
-    (response) => {
-      response.json().then((data) => {
-          console.log("OPEN LIBRARY DATA");
-          console.log(data);
-      });
-    }
-  );
-
