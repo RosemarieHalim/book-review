a1a31b52c38588fd8ed12c25c136f7bbc5824338
IsaiasQB <isaias.qb@hotmail.com>
2022-03-31 20:20:18 -0400

    added html for the main container

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,7 @@
-let input = "love";
-fetch(`https://www.googleapis.com/books/v1/volumes/?q=${input}`).then(
-  (response) => {
-    response.json().then((data) => {
-        console.log("gOOGLE DATA");
-        console.log(data);
-    });
-  }
-);
 
-fetch("https://www.googleapis.com/books/v1/volumes?q=intitle:life+after").then(
+
+// example of an API call from google
+fetch("https://www.googleapis.com/books/v1/volumes?q=intitle:philosopher+stone&printType=books&maxResults=40").then(
     (response) => {
       response.json().then((data) => {
           console.log("gOOGLE DATA in tittle");
@@ -17,16 +10,14 @@ fetch("https://www.googleapis.com/books/v1/volumes?q=intitle:life+after").then(
     }
   );
 
+  
+// example of an API call from open library using ISBN:9780756413712 identifier as parameter
+fetch("https://openlibrary.org/api/books?bibkeys=ISBN:9780756413712&jscmd=details&format=json").then(
+    (response) => {
+      response.json().then((data) => {
+          console.log("OPEN LIBRARY DATA");
+          console.log(data);
+      });
+    }
+  );
 
-let key = "clDs6fwfqKr0Y0PRfKe3UbdrkvPt6wwd";
-
-var bookName = 
-
-fetch("https://api.nytimes.com/svc/books/v3/reviews.json?title=life&api-key="+key).then(
-  (response) => {
-    response.json().then((data) => {
-        console.log("NYT DATA");
-        console.log(data);
-    });
-  }
-);
