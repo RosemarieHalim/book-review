8dbd34d697acb51fca73114e38b9bbcb0fa24810
IsaiasQB <isaias.qb@hotmail.com>
2022-04-01 19:41:50 -0400

    merged work form 3 teammates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 
-// Variables from DOM
+    // Variables from DOM
+var bookNameInput = document.querySelector(".input")
+
 var bookInfoEl = document.querySelector("#book-info-container") //the general information container for the book
 var bookTitleEl = document.querySelector("#book-title")
 
@@ -11,6 +13,39 @@ var bookTagAuthorEl = document.querySelector("#book-tag-author")
 var bookTagTopicEl = document.querySelector("#book-tag-topic")
 var bookTagLanguageEl = document.querySelector("#book-tag-language")
  
+    // API call
+fetch(
+    `https://www.googleapis.com/books/v1/volumes/?q=intitle:harry+potter`
+  ).then((response) => {
+    response.json().then((data) => {
+    //   console.log(data);
+  
+      var arrayOfBooks = [];
+  
+      data.items.forEach((book) => {
+        var bookDetails = {
+          title: book.volumeInfo.title,
+          authors: book.volumeInfo.authors,
+          imageLink: book.volumeInfo.imageLinks.thumbnail,
+          description: book.volumeInfo.description,
+          genre: book.volumeInfo.categories,
+          averageRating: book.volumeInfo.averageRating + " Out Of 5 Stars",
+          numberOfRatings: book.volumeInfo.ratingsCount,
+        };
+        arrayOfBooks.push(bookDetails);
+        console.log(bookDetails)
+      });
+    });
+  });
+
+// BURGER ELEMENT FUNCTION
+function burger(x) {
+    x.classList.toggle("change");
+}
+
+
+
+
 //     // example of an API call from google, serching by the words included in the Book Tile 
 // fetch("https://www.googleapis.com/books/v1/volumes?q=intitle:the+name+wind&printType=books&maxResults=40").then(
 //     (response) => {
@@ -32,8 +67,3 @@ var bookTagLanguageEl = document.querySelector("#book-tag-language")
 //       });
 //     }
 //   );
-
-// BURGER ELEMENT FUNCTION
-function burger(x) {
-    x.classList.toggle("change");
-}
