30a61bf407a25c6fe40befa09e89b51adca2fba7
Andrew Howran <andrewhowrandev17@gmail.com>
2022-04-01 20:11:39 -0400

    captured and formatted user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,60 @@
-fetch(
-  `https://www.googleapis.com/books/v1/volumes/?q=intitle:harry+potter`
-).then((response) => {
-  response.json().then((data) => {
-    console.log(data);
-
-    var arrayOfBooks = [];
-
-    data.items.forEach((book) => {
-      var bookDetails = {
-        title: book.volumeInfo.title,
-        authors: book.volumeInfo.authors,
-        imageLink: book.volumeInfo.imageLinks.thumbnail,
-        description: book.volumeInfo.description,
-        genre: book.volumeInfo.categories,
-        averageRating: book.volumeInfo.averageRating + " Out Of 5 Stars",
-        numberOfRatings: book.volumeInfo.ratingsCount,
-      };
-      arrayOfBooks.push(bookDetails);
-    });
-  });
+// listen for a submit event on the form
+$("#myForm").on("submit", function (event) {
+  //prevent the form from submitting
+  event.preventDefault();
+  //check if the input value is empty before continuing
+  if ($("#myInput").val()) {
+    //capture the users input
+    var userInput = $("#myInput").val().trim();
+    //clear the form input
+    $("#myInput").val("");
+    //format user input
+    userInput = formatUserInput(userInput);
+    getBookData(userInput);
+  }
 });
+
+function getBookData(input) {
+  var arrayOfBooks = [];
+
+  fetch(`https://www.googleapis.com/books/v1/volumes/?q=intitle:${input}`)
+    .then((response) => {
+      response.json().then((data) => {
+        data.items.forEach((book) => {
+          var bookDetails = {
+            title: book.volumeInfo.title,
+            authors: book.volumeInfo.authors,
+            imageLink: book.volumeInfo.imageLinks,
+            description: book.volumeInfo.description,
+            genre: book.volumeInfo.categories,
+            averageRating: book.volumeInfo.averageRating + " Out Of 5 Stars",
+            numberOfRatings: book.volumeInfo.ratingsCount,
+          };
+          arrayOfBooks.push(bookDetails);
+        });
+      });
+      console.log(arrayOfBooks);
+
+      $("#mydiv").html("<img></img>").class
+      ;
+    })
+    .catch((error) => {
+      //some kind of error alert goes here, maybe a modal or just some red text under the form input element
+    });
+}
+
+function formatUserInput(input) {
+  //turn the string into an array
+  var inputAsArray = input.split(" ");
+  var formattedInput = "";
+  //add a "+" to the end of each word except for the last word
+  for (i = 0; i < inputAsArray.length; i++) {
+    //if i + 1 is === inputAsArray.length we are at the last word, so only add the word then continue to end the loop
+    if (i + 1 === inputAsArray.length) {
+      formattedInput += inputAsArray[i];
+      continue;
+    }
+    formattedInput += inputAsArray[i] + "+";
+  }
+  return formattedInput;
+}
