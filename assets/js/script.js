da0c19c5422a087f14dbaf61e521c4c25ce99a1e
IsaiasQB <isaias.qb@hotmail.com>
2022-04-03 21:25:08 -0400

    adding aditional styling to the book infomration cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,7 +19,7 @@ fetch("https://www.googleapis.com/books/v1/volumes?q=intitle:wolf+winter&printTy
             genre: book.volumeInfo.categories[0],
             averageRating: book.volumeInfo.averageRating + " Out Of 5 Stars",
             published: book.volumeInfo.publishedDate,
-            isbn: book.items[0].volumeInfo.industryIdentifiers[1].industryIdentifier
+            // isbn: book.items[0].volumeInfo.industryIdentifiers[1].industryIdentifier
         };
         arrayOfBooks.push(bookDetails);
         createList();  
@@ -33,21 +33,21 @@ var createList = function (){
         console.log(arrayOfBooks[i].title)
 
     var bookInfoContainer = document.createElement("div")
-        bookInfoContainer.className = "book-info-container card"; //container for each book info
+        bookInfoContainer.className = "book-info-container card pb-3git add"; //container for each book info
 
         var bookTitleEl = document.createElement("h2")
-        bookTitleEl.className = "book-title card-header card-header-title";
+        bookTitleEl.className = "book-title card-header card-header-title is-size-2";
         bookTitleEl.textContent= arrayOfBooks[i].title    //and summaryTitleEl
 
         var summaryTitleEl = document.createElement("h3")
-        summaryTitleEl.className = "p-3 summary-title"
-        summaryTitleEl.innerText = "Summary"
+        summaryTitleEl.className = "p-3 summary-title is-size-3"
+        summaryTitleEl.innerText = "Summary" 
 
 
         var bookSummaryContainer = document.createElement("div")
         bookSummaryContainer.className = "summary-body p-3" //container for the summary and book cover
             var bookSummaryEl = document.createElement("p")
-            bookSummaryEl.className = "book-summary"
+            bookSummaryEl.className = "is-size-5"
             bookSummaryEl.textContent= arrayOfBooks[i].description
 
             var bookCoverEl = document.createElement("img")
@@ -58,22 +58,22 @@ var createList = function (){
         bookTagsContainer.className = "book-tags-container card-footer" 
             var tagGenreEl = document.createElement("p")
                 tagGenreEl.className = "book-tag"
-                tagGenreEl.innerHTML = "<p>Genre</p></br><tag class=is-danger>"+ arrayOfBooks[i].genre +"</tag>"
+                tagGenreEl.innerHTML = "<p>Genre</p></br><tag>"+ arrayOfBooks[i].genre +"</tag>"
             var tagAuthorEl = document.createElement("p")
                 tagAuthorEl.className = "book-tag"
-                tagAuthorEl.innerHTML = "<p>Author</p></br><tag class=is-danger>"+ arrayOfBooks[i].authors +"</tag>"
+                tagAuthorEl.innerHTML = "<p>Author</p></br><tag>"+ arrayOfBooks[i].authors +"</tag>"
             var tagPublishedEl = document.createElement("p")
                 tagPublishedEl.className = "book-tag"
-                tagPublishedEl.innerHTML = "<p>Published:</p></br><tag class=is-danger>"+ arrayOfBooks[i].published +"</tag>"
+                tagPublishedEl.innerHTML = "<p>Published:</p></br><tag>"+ arrayOfBooks[i].published +"</tag>"
             var tagRatingEl = document.createElement("p")
                 tagRatingEl.className = "book-tag"
-                tagRatingEl.innerHTML = "<p>Ratings</p></br><tag class=is-danger>"+ arrayOfBooks[i].averageRating +"</tag>"
-            var tagIsbnEl = document.createElement("p")
-                tagIsbnEl.className = "book-tag"
-                tagIsbnEl.innerHTML = "<p>Genre</p></br><tag class=is-danger>"+ arrayOfBooks[i].isbn +"</tag>"
+                tagRatingEl.innerHTML = "<p>Ratings</p></br><tag>"+ arrayOfBooks[i].averageRating +"</tag>"
+            // var tagIsbnEl = document.createElement("p")
+            //     tagIsbnEl.className = "book-tag"
+            //     tagIsbnEl.innerHTML = "<p class=is-danger>Genre</p></br><tag class=is-danger>"+ arrayOfBooks[i].isbn +"</tag>"
 
     bookSummaryContainer.append(bookSummaryEl, bookCoverEl)
-    bookTagsContainer.append(tagGenreEl, tagAuthorEl, tagPublishedEl, tagRatingEl, tagIsbnEl)
+    bookTagsContainer.append(tagGenreEl, tagAuthorEl, tagPublishedEl, tagRatingEl ) // perhaps adding tagIsbnEl
     bookInfoContainer.append(bookTitleEl, summaryTitleEl, bookSummaryContainer, bookTagsContainer)
     bookListContainer.append(bookInfoContainer)
     }//for loop ends
