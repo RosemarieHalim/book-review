421964400870c0c0ff7a7a4156483b4b7d5681e7
Andrew Howran <andrewhowrandev17@gmail.com>
2022-04-05 14:06:40 -0400

    added book list

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,9 @@
-// Selecting the section where the book information goes
-var bookListContainer = document.querySelector(".book-profiles-container"); // container for all the listed books
-
-// saves the values of the data retreived fomr the call
-var arrayOfBooks = [];
-
 //API call
 function getBookData(bookString) {
-  console.log(bookString);
+  // saves the values of the data retreived fomr the call
+  var arrayOfBooks = [];
   fetch(
-    `https://www.googleapis.com/books/v1/volumes?q=intitle:${bookString}&printType=books&maxResults=3`
+    `https://www.googleapis.com/books/v1/volumes?q=intitle:${bookString}&printType=books&maxResults=16`
   ).then((response) => {
     response.json().then((data) => {
       console.log(data);
@@ -21,80 +16,41 @@ function getBookData(bookString) {
           genre: book.volumeInfo.categories[0],
           averageRating: book.volumeInfo.averageRating + " Out Of 5 Stars",
           published: book.volumeInfo.publishedDate,
+          textSnippet: book.searchInfo.textSnippet,
           // isbn: book.items[0].volumeInfo.industryIdentifiers[1].industryIdentifier
         };
         arrayOfBooks.push(bookDetails);
-        createList();
+        //createList();
       });
+      createListTwo(arrayOfBooks);
     });
   });
 }
 
-// create a section/card for every book from the API call
-var createList = function () {
-  for (let i = 0; i < arrayOfBooks.length; i++) {
-    console.log(arrayOfBooks[i].title);
+function createListTwo(bookData) {
+  console.log(bookData);
+  //create the three main containers for books and add bluma classes
+  var mainBookListContainer = $("<div></div>").addClass("container");
+  var columnsContainer = $("<div></div>").addClass(
+    "columns is-multiline mt-6 has-text-centered is-family-primary"
+  );
+  $("#main-book-list-container").append(mainBookListContainer);
+  mainBookListContainer.append(columnsContainer);
 
-    var bookInfoContainer = document.createElement("div");
-    bookInfoContainer.className = "book-info-container card pb-3git add"; //container for each book info
+  //loop through the book array
+  for (i = 0; i < bookData.length; i++) {
+    var bookContainer = $("<div>").addClass("column is-6-tablet is-3-desktop");
+    var bookImg = $("<img>").attr("src", bookData[i].imageLink).addClass("shadow");
+    var bookTitle = $("<h2>").text(bookData[i].title);
+    var bookAuthor = $("<h3>").text(bookData[i].authors);
 
-    var bookTitleEl = document.createElement("h2");
-    bookTitleEl.className =
-      "book-title card-header card-header-title is-size-2";
-    bookTitleEl.textContent = arrayOfBooks[i].title; //and summaryTitleEl
+    columnsContainer.append(bookContainer);
 
-    var summaryTitleEl = document.createElement("h3");
-    summaryTitleEl.className = "p-3 summary-title is-size-3";
-    summaryTitleEl.innerText = "Summary";
-
-    var bookSummaryContainer = document.createElement("div");
-    bookSummaryContainer.className = "summary-body p-3"; //container for the summary and book cover
-    var bookSummaryEl = document.createElement("p");
-    bookSummaryEl.className = "is-size-5";
-    bookSummaryEl.textContent = arrayOfBooks[i].description;
-
-    var bookCoverEl = document.createElement("img");
-    bookCoverEl.setAttribute("src", arrayOfBooks[i].imageLink);
-
-    // content for bookTagsContainer
-    var bookTagsContainer = document.createElement("div");
-    bookTagsContainer.className = "book-tags-container card-footer";
-    var tagGenreEl = document.createElement("p");
-    tagGenreEl.className = "book-tag";
-    tagGenreEl.innerHTML =
-      "<p>Genre</p></br><tag>" + arrayOfBooks[i].genre + "</tag>";
-    var tagAuthorEl = document.createElement("p");
-    tagAuthorEl.className = "book-tag";
-    tagAuthorEl.innerHTML =
-      "<p>Author</p></br><tag>" + arrayOfBooks[i].authors + "</tag>";
-    var tagPublishedEl = document.createElement("p");
-    tagPublishedEl.className = "book-tag";
-    tagPublishedEl.innerHTML =
-      "<p>Published:</p></br><tag>" + arrayOfBooks[i].published + "</tag>";
-    var tagRatingEl = document.createElement("p");
-    tagRatingEl.className = "book-tag";
-    tagRatingEl.innerHTML =
-      "<p>Ratings</p></br><tag>" + arrayOfBooks[i].averageRating + "</tag>";
-    // var tagIsbnEl = document.createElement("p")
-    //     tagIsbnEl.className = "book-tag"
-    //     tagIsbnEl.innerHTML = "<p class=is-danger>Genre</p></br><tag class=is-danger>"+ arrayOfBooks[i].isbn +"</tag>"
-
-    bookSummaryContainer.append(bookSummaryEl, bookCoverEl);
-    bookTagsContainer.append(
-      tagGenreEl,
-      tagAuthorEl,
-      tagPublishedEl,
-      tagRatingEl
-    ); // perhaps adding tagIsbnEl
-    bookInfoContainer.append(
-      bookTitleEl,
-      summaryTitleEl,
-      bookSummaryContainer,
-      bookTagsContainer
-    );
-    bookListContainer.append(bookInfoContainer);
-  } //for loop ends
-};
+    bookContainer.append(bookImg);
+    bookContainer.append(bookTitle);
+    bookContainer.append(bookAuthor);
+  }
+}
 
 // BURGER ELEMENT FUNCTION
 function burger(x) {
@@ -132,3 +88,73 @@ function formatUserInput(input) {
   }
   return formattedInput;
 }
+
+//------------------------------------------------------------------------------
+// Selecting the section where the book information goes
+// var bookListContainer = document.querySelector(".book-profiles-container"); // container for all the listed books
+
+// create a section/card for every book from the API call
+// var createList = function () {
+//   for (let i = 0; i < arrayOfBooks.length; i++) {
+
+//     var bookInfoContainer = document.createElement("div");
+//     bookInfoContainer.className = "book-info-container card pb-3git add"; //container for each book info
+
+//     var bookTitleEl = document.createElement("h2");
+//     bookTitleEl.className =
+//       "book-title card-header card-header-title is-size-2";
+//     bookTitleEl.textContent = arrayOfBooks[i].title; //and summaryTitleEl
+
+//     var summaryTitleEl = document.createElement("h3");
+//     summaryTitleEl.className = "p-3 summary-title is-size-3";
+//     summaryTitleEl.innerText = "Summary";
+
+//     var bookSummaryContainer = document.createElement("div");
+//     bookSummaryContainer.className = "summary-body p-3"; //container for the summary and book cover
+//     var bookSummaryEl = document.createElement("p");
+//     bookSummaryEl.className = "is-size-5";
+//     bookSummaryEl.textContent = arrayOfBooks[i].description;
+
+//     var bookCoverEl = document.createElement("img");
+//     bookCoverEl.setAttribute("src", arrayOfBooks[i].imageLink);
+
+//     // content for bookTagsContainer
+//     var bookTagsContainer = document.createElement("div");
+//     bookTagsContainer.className = "book-tags-container card-footer";
+//     var tagGenreEl = document.createElement("p");
+//     tagGenreEl.className = "book-tag";
+//     tagGenreEl.innerHTML =
+//       "<p>Genre</p></br><tag>" + arrayOfBooks[i].genre + "</tag>";
+//     var tagAuthorEl = document.createElement("p");
+//     tagAuthorEl.className = "book-tag";
+//     tagAuthorEl.innerHTML =
+//       "<p>Author</p></br><tag>" + arrayOfBooks[i].authors + "</tag>";
+//     var tagPublishedEl = document.createElement("p");
+//     tagPublishedEl.className = "book-tag";
+//     tagPublishedEl.innerHTML =
+//       "<p>Published:</p></br><tag>" + arrayOfBooks[i].published + "</tag>";
+//     var tagRatingEl = document.createElement("p");
+//     tagRatingEl.className = "book-tag";
+//     tagRatingEl.innerHTML =
+//       "<p>Ratings</p></br><tag>" + arrayOfBooks[i].averageRating + "</tag>";
+//     // var tagIsbnEl = document.createElement("p")
+//     //     tagIsbnEl.className = "book-tag"
+//     //     tagIsbnEl.innerHTML = "<p class=is-danger>Genre</p></br><tag class=is-danger>"+ arrayOfBooks[i].isbn +"</tag>"
+
+//     bookSummaryContainer.append(bookSummaryEl, bookCoverEl);
+//     bookTagsContainer.append(
+//       tagGenreEl,
+//       tagAuthorEl,
+//       tagPublishedEl,
+//       tagRatingEl
+//     ); // perhaps adding tagIsbnEl
+//     bookInfoContainer.append(
+//       bookTitleEl,
+//       summaryTitleEl,
+//       bookSummaryContainer,
+//       bookTagsContainer
+//     );
+//     bookListContainer.append(bookInfoContainer);
+//   } //for loop ends
+// };
+//---------------------------------------------------------------------------------------
