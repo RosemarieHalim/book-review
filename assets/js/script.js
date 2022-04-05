2ec55bba5a34d5a1885bfbfa4e4db5d7431ccd14
Andrew Howran <andrewhowrandev17@gmail.com>
2022-04-05 19:26:26 -0400

    updated css

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,7 @@ function getBookData(bookString) {
   // saves the values of the data retreived fomr the call
   var arrayOfBooks = [];
   fetch(
-<<<<<<< HEAD
     `https://www.googleapis.com/books/v1/volumes?q=intitle:${bookString}&printType=books&maxResults=16`
-=======
-    `https://www.googleapis.com/books/v1/volumes?q=intitle:${bookString}&printType=books&maxResults=15`
->>>>>>> 25e496e37cb1c117763e7e805128a2fdb35268ca
   ).then((response) => {
     response.json().then((data) => {
       console.log(data);
@@ -41,7 +37,6 @@ function createListTwo(bookData) {
   $("#main-book-list-container").append(mainBookListContainer);
   mainBookListContainer.append(columnsContainer);
 
-<<<<<<< HEAD
   //loop through the book array
   for (i = 0; i < bookData.length; i++) {
     var bookContainer = $("<div>").addClass("column is-6-tablet is-3-desktop");
@@ -56,67 +51,6 @@ function createListTwo(bookData) {
     bookContainer.append(bookAuthor);
   }
 }
-=======
-    var bookInfoContainer = document.createElement("div")
-        bookInfoContainer.className = "book-info-container card py-3 mb-5"; //container for each book info
-
-        var bookTitleEl = document.createElement("h2")
-        bookTitleEl.className = "book-title card-header card-header-title is-size-2 py-4";
-        bookTitleEl.textContent= arrayOfBooks[i].title    //and summaryTitleEl
-
-    var summaryTitleEl = document.createElement("h3");
-    summaryTitleEl.className = "p-3 summary-title is-size-3";
-    summaryTitleEl.innerText = "Summary";
-
-    var bookSummaryContainer = document.createElement("div");
-    bookSummaryContainer.className = "summary-body p-3"; //container for the summary and book cover
-    var bookSummaryEl = document.createElement("p");
-    bookSummaryEl.className = "is-size-5";
-    bookSummaryEl.textContent = arrayOfBooks[i].description;
-
-    var bookCoverEl = document.createElement("img");
-    bookCoverEl.setAttribute("src", arrayOfBooks[i].imageLink);
-
-    // content for bookTagsContainer
-    var bookTagsContainer = document.createElement("div");
-    bookTagsContainer.className = "book-tags-container card-footer py-3";
-    var tagGenreEl = document.createElement("p");
-    tagGenreEl.className = "book-tag";
-    tagGenreEl.innerHTML =
-      "<p>Genre</p></br><tag>" + arrayOfBooks[i].genre + "</tag>";
-    var tagAuthorEl = document.createElement("p");
-    tagAuthorEl.className = "book-tag";
-    tagAuthorEl.innerHTML =
-      "<p>Author</p></br><tag>" + arrayOfBooks[i].authors + "</tag>";
-    var tagPublishedEl = document.createElement("p");
-    tagPublishedEl.className = "book-tag";
-    tagPublishedEl.innerHTML =
-      "<p>Published:</p></br><tag>" + arrayOfBooks[i].published + "</tag>";
-    var tagRatingEl = document.createElement("p");
-    tagRatingEl.className = "book-tag";
-    tagRatingEl.innerHTML =
-      "<p>Ratings</p></br><tag>" + arrayOfBooks[i].averageRating + "</tag>";
-    // var tagIsbnEl = document.createElement("p")
-    //     tagIsbnEl.className = "book-tag"
-    //     tagIsbnEl.innerHTML = "<p class=is-danger>Genre</p></br><tag class=is-danger>"+ arrayOfBooks[i].isbn +"</tag>"
-
-    bookSummaryContainer.append(bookSummaryEl, bookCoverEl);
-    bookTagsContainer.append(
-      tagGenreEl,
-      tagAuthorEl,
-      tagPublishedEl,
-      tagRatingEl
-    ); // perhaps adding tagIsbnEl
-    bookInfoContainer.append(
-      bookTitleEl,
-      summaryTitleEl,
-      bookSummaryContainer,
-      bookTagsContainer
-    );
-    bookListContainer.append(bookInfoContainer);
-  } //for loop ends
-};
->>>>>>> 25e496e37cb1c117763e7e805128a2fdb35268ca
 
 // BURGER ELEMENT FUNCTION
 function burger(x) {
